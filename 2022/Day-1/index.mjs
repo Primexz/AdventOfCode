bf13cddf2b46e2c7f5f7631d30f715bc5ba687e2
Primexz <johann@pepebot.info>
2023-11-21 09:11:33 +0100

    chore(2022-1): simplify

--- 2022/Day-1/index.mjs
+++ 2022/Day-1/index.mjs
@@ -6,20 +6,18 @@ data = data
   .split("\n\n")
   .map((elv) => elv.split("\n").map((calory) => parseInt(calory, 10)));
 
-let out = [];
-data.forEach((elv) => {
-  out.push(
-    elv.reduce((acc, cur) => {
-      acc += cur;
-      return acc;
-    }, 0)
-  );
-});
+const out = data.reduce((acc, curr) => {
+  acc.push(curr.reduce((acc, curr) => acc + curr, 0));
+  return acc;
+}, []);
 
 console.log("highest calory: ", Math.max(...out));
 
-const sortedOut = out.sort((a, b) => a - b).reverse();
 console.log(
   "top 3: ",
-  sortedOut.slice(0, 3).reduce((acc, cur) => acc + cur, 0)
+  out
+    .sort((a, b) => a - b)
+    .reverse()
+    .slice(0, 3)
+    .reduce((acc, cur) => acc + cur, 0)
 );
